c85b839521846eda417f43558c67d3d297f57677
Mumtaz Khan <makcalif@yahoo.com>
2019-12-19 20:56:57 -0600

    fixed lambda syntax errors

--- dummyTranscribe.js
+++ dummyTranscribe.js
@@ -3,10 +3,10 @@ console.log('Loading function');
 const aws = require('aws-sdk'); 
 
 exports.handle = async (event) => {
- 
+    console.log(`event : ${JSON.stringify(event)}`);
     if (!event.ucid) {
         throw (new Error('missing ucid'));
     } 
     // write to cloud watch
-    console.log(`transcribe complete for ucid: ${ucid}`)
+    console.log(`transcribe complete for ucid: ${event.ucid}`)
 }
--- submitTranscribe.js
+++ submitTranscribe.js
@@ -1,33 +1,39 @@
 'use strict';
 console.log('Loading function');
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
 const lambda = new AWS.Lambda();
 
 exports.submit = async (event) => {
  
-    // call dummy transcribe lambda
-    var params = {
-        FunctionName: 'transcribe-dev-dummyTranscribe', /* required */
-        Payload: JSON.stringify({"ucid": event.ucid})
-      };
-      
-    const invokeLambda = (lambda, params) => new Promise((resolve, reject) => {
-    lambda.invoke(params, (error, data) => {
-            if (error) {
-            reject(error);
-            } else {
-            resolve(data);
-            }
+    if (event.ucid) {
+        // call dummy transcribe lambda
+        var params = {
+            FunctionName: 'transcribe-dev-dummyTranscribe', /* required */
+            Payload: JSON.stringify({"ucid": event.ucid})
+        };
+        
+        const invokeLambda = (lambda, params) => new Promise((resolve, reject) => {
+        lambda.invoke(params, (error, data) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+            });
         });
-    });
 
-    try {
-        const result = await invokeLambda(lambda, params);
+        try {
+            const result = await invokeLambda(lambda, params);
 
-        console.log('Success!');
-        console.log(result);
+            console.log('Success!');
+            console.log(result);
+        }
+        catch(err) {
+            console.log(`err : ${err}`);
+        };
     }
-    catch(err) {
-        console.log(`err : ${err}`);
-    };
+    else {
+        throw (new Error('missing ucid'));
+    }
+    
 }
